c6fb359a25939502147824422a529279caded88a
Marcio Araya <marcio.araya@gmail.com>
2024-04-16 11:12:55 -0300

    altera verifica.php

--- aula08-exercicio/verifica.php
+++ aula08-exercicio/verifica.php
@@ -1,11 +1,6 @@
 <?php
 if (!isset($_COOKIE["usuario"])) {
     header('location:login.php');
-    /*
-    echo '<p>É necessário fazer o login para acessar o sistema</p>';
-    echo '<a href="login.php">Ir para login</a>';
-    */
-
 }
 
 ?>
